b28520aa57676da856158c510792fa9be6bd42ac
Gavin Broekema <gavinbroekema@gmail.com>
2023-03-02 18:49:57 -0600

    Photo data undefined in ImageList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom'
+
+import apiKey from './config';
+import ImageList from './ImageList';
+import Nav from './Nav';
+
+
+
+const tag = ''
+
 function App() {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('cats')
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let activeFetch = true;
+    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=762409951bbbdaa6ec50945c227e49de&tags=flowers&per_page=24&page=1&format=json&nojsoncallback=1`)
+      .then(res => res.json())
+      .then(result => {
+        setImages(result);
+        setLoading(false);
+      })
+      .catch(err => console.log('Error parsing data', err))
+    return () => {activeFetch = false}
+  }, [query])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<ImageList data={images} />}></Route>
+    </Routes>
   );
 }
 
---
+++ src/Image.js
@@ -0,0 +1,15 @@
+// const Image = (props) => {
+//   // const {images} = props;
+//   const {url,id,title} = props;
+//   return (
+//     <li>
+//       <img
+//           src={url}
+//           key={id}
+//           alt={}
+//           />
+//     </li>
+//   )
+// }
+
+// export default Image;
